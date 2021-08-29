e98194af6fb179d6c4968a97e947c2bae475c033
Memecy <Memecy@users.noreply.github.com>
2021-08-29 16:41:33 +0300

    First commit

---
+++ com.cucumlips.maven.eclipse/src/main/java/com/cucumlips/maven/eclipse/App.java
@@ -0,0 +1,16 @@
+package com.cucumlips.maven.eclipse;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
+}
